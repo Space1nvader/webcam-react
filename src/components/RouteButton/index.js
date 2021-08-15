282ed9b74ae319c030604010a060d2f7636fd763
Pavel <buteafulspace@gmail.com>
2021-08-15 20:28:56 +0400

    fix route btn classname

--- src/components/RouteButton/index.js
+++ src/components/RouteButton/index.js
@@ -4,13 +4,15 @@ import clsx from 'clsx';
 import './index.scss';
 
 const RouteButton = (props) => {
-  const { children, to, icon, className, ...other } = props;
+  const { children, to, icon, className = '', ...other } = props;
+  // TODO: ACTIVE CILDREN
+  const isActiveRoute = (match, location) => console.log(match, location);
   return (
     <NavLink
-      type="button"
       to={to}
       activeClassName="active"
-      className={clsx(`RouteButton ${className}`)}
+      // isActive={(match, location) => isActiveRoute(match, location)}
+      className={clsx(`routeButton ${className}`)}
       {...other}
     >
       {icon && icon}
